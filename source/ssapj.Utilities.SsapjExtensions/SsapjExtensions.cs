317a6775853abb89ae9a2e176c993140c777ccf1
Takeo Oyama <40187858+seagulltoyama@users.noreply.github.com>
2019-06-28 16:13:36 +0900

    initial

---
+++ source/ssapj.Utilities.SsapjExtensions/SsapjExtensions.cs
@@ -0,0 +1,17 @@
+﻿using BarTender;
+using System.Collections.Generic;
+// ReSharper disable CheckNamespace
+
+namespace ssapj.Utilities
+{
+    public static class SsapjExtensions
+    {
+        public static IEnumerable<DesignObject> ToEnumerable(this DesignObjects designObjects)
+        {
+            foreach (DesignObject designObject in designObjects)
+            {
+                yield return designObject;
+            }
+        }
+    }
+}
